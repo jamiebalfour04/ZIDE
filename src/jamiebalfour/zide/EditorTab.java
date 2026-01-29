14697aa01a060dfb48b7ff3f3fbea5a34a3df447
Jamie Balfour <jamie@balfour.scot>
2026-01-29 22:04:26 +0000

    UI improvements for dark mode.

--- src/jamiebalfour/zide/EditorTab.java
+++ src/jamiebalfour/zide/EditorTab.java
@@ -2,41 +2,122 @@ package jamiebalfour.zide;
 
 import jamiebalfour.codeeditor.CodeEditorView;
 import jamiebalfour.ui.BalfLafManager;
+import jamiebalfour.ui.components.BalfScrollbar;
 import javafx.scene.control.Tab;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class EditorTab extends Tab {
   private final String path;
   private final jamiebalfour.codeeditor.CodeEditorView editor;
+  private final BalfScrollbar pane;
 
-  public EditorTab(String title, String path, jamiebalfour.codeeditor.CodeEditorView editor, javafx.scene.Node content) {
+  public EditorTab(String title, String path, jamiebalfour.codeeditor.CodeEditorView editor, BalfScrollbar pane, javafx.scene.Node content) {
     super(title, content);
     this.path = path;
     this.editor = editor;
+    this.pane = pane;
 
-    BalfLafManager.getInstance().addThemeChangeListener(new BalfLafManager.ThemeChangeListener() {
-
-      @Override
-      public void onThemeChanged() {
-        if(BalfLafManager.getInstance().isDarkModeEnabled()){
-          editor.setBackground(new Color(30, 30, 30));
-          editor.setForeground(Color.white);
-          editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Normal, Color.white);
-          editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Quote,    new Color(198, 120, 221)); // Soft purple
-          editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Keyword,  new Color(255, 85, 114));  // Strong coral pink
-          editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Function, new Color(97, 175, 239));  // Bright sky blue
-          editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Var,      new Color(102, 217, 239)); // Aqua cyan
-          editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Type,     new Color(229, 192, 123)); // Sand/gold
-          editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Bool,     new Color(189, 147, 249)); // Light lavender
-          editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Special,     new Color(0, 200, 0)); // Green
-        }
+    BalfLafManager.getInstance().addThemeChangeListener(() -> {
+      if(BalfLafManager.getInstance().isDarkModeEnabled()){
+        editor.setBackground(new Color(0, 0, 0));
+        editor.setForeground(Color.white);
+        editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Normal, Color.white);
+        editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Quote,    new Color(198, 120, 221)); // Soft purple
+        editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Keyword,  new Color(255, 85, 114));  // Strong coral pink
+        editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Function, new Color(97, 175, 239));  // Bright sky blue
+        editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Var,      new Color(102, 217, 239)); // Aqua cyan
+        editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Type,     new Color(229, 192, 123)); // Sand/gold
+        editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Bool,     new Color(189, 147, 249)); // Light lavender
+        editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Special,     new Color(0, 200, 0)); // Green
+        editor.repaint();
+        editor.setText(editor.getText());
       }
     });
   }
+  
 
   public String getPath() {
     return path;
   }
+
   public jamiebalfour.codeeditor.CodeEditorView getEditor() { return editor; }
+  public BalfScrollbar getScrollPane() { return pane; }
+
+  void switchOnDarkMode() {
+
+
+    Color dark = Color.decode("#282D37");
+
+
+    BalfLafManager.getInstance().toggleDarkMode(true);
+
+    pane.setDarkColour(dark);
+
+    editor.setBackground(dark);
+    editor.setForeground(Color.WHITE);
+    editor.setCaretColor(Color.WHITE);
+
+    editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Normal, Color.white);
+    /*editor.setAttributeColor(ATTR_TYPE.Quote, new Color(152, 195, 119));
+    editor.setAttributeColor(ATTR_TYPE.Keyword, new Color(255, 123, 114));
+    editor.setAttributeColor(ATTR_TYPE.Function, new Color(210, 168, 255));
+    editor.setAttributeColor(ATTR_TYPE.Var, new Color(0, 106, 44));
+    editor.setAttributeColor(ATTR_TYPE.Type, new Color(121, 192, 255));
+    editor.setAttributeColor(ATTR_TYPE.Bool, new Color(208, 154, 102));*/
+
+    editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Quote,    new Color(198, 120, 221)); // Soft purple
+    editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Keyword,  new Color(255, 85, 114));  // Strong coral pink
+    editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Function, new Color(97, 175, 239));  // Bright sky blue
+    editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Var,      new Color(102, 217, 239)); // Aqua cyan
+    editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Type,     new Color(229, 192, 123)); // Sand/gold
+    editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Bool,     new Color(189, 147, 249)); // Light lavender
+    editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Special,     new Color(0, 200, 0)); // Green
+
+
+
+    //autoCompleteItemForeColor = new Color(255, 255, 255, 255);
+    editor.setAutoCompleteItemBackgroundColor(new Color(30, 39, 75, 255));
+    resetScroll();
+
+  }
+
+  void switchOffDarkMode() {
+
+
+    Color light = new Color(255, 255, 255);
+    pane.setLightColour(Color.white);
+    editor.setBackground(light);
+    editor.setForeground(Color.black);
+    editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Normal, Color.black);
+    editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Quote, new Color(0, 128, 0));
+    editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Keyword, new Color(135, 16, 148));
+    editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Function, new Color(97, 172, 231));
+    editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Var, new Color(255, 138, 0));
+    editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Type, new Color(2, 87, 172));
+    editor.setAttributeColor(CodeEditorView.ATTR_TYPE.Special, new Color(255, 0, 0)); // Pink
+    editor.setCaretColor(Color.black);
+    //autoCompleteItemForeColor = new Color(31, 31, 31, 255);
+    //autoCompleteItemBackgroundColor = new Color(241, 241, 241, 255);
+
+    editor.setAutoCompleteItemBackgroundColor(new Color(241, 241, 241, 255));
+
+    resetScroll();
+    BalfLafManager.getInstance().toggleDarkMode(false);
+
+  }
+
+
+  private void resetScroll() {
+    int caretPosition = editor.getCaretPosition();
+    int scrollPosition = pane.getVerticalScrollBar().getValue();
+    editor.setText(editor.getText());
+    SwingUtilities.invokeLater(() -> {
+      editor.requestFocus();
+      editor.setCaretPosition(caretPosition);
+      pane.getVerticalScrollBar().setValue(scrollPosition);
+    });
+  }
+
 }
--- src/jamiebalfour/zide/ZIDEMain.java
+++ src/jamiebalfour/zide/ZIDEMain.java
@@ -1,13 +1,13 @@
 package jamiebalfour.zide;
 
-import com.sun.javafx.tk.FontLoader;
 import jamiebalfour.FileHelperFunctions;
-import jamiebalfour.HelperFunctions;
 import jamiebalfour.balflaf_fx.BalfTitleBar;
 import jamiebalfour.balflaf_fx.WindowResizer;
 import jamiebalfour.codeeditor.CodeEditorView;
 import jamiebalfour.ui.BalfLafManager;
+import jamiebalfour.ui.components.BalfPanel;
 import jamiebalfour.ui.components.BalfScrollbar;
+import jamiebalfour.ui.components.BalfSearchBox;
 import jamiebalfour.zpe.core.ZPEHelperFunctions;
 import jamiebalfour.zpe.core.ZPEInstance;
 import jamiebalfour.zpe.core.ZPEKit;
@@ -28,7 +28,9 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCombination;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -43,8 +45,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
@@ -58,6 +59,8 @@ public class ZIDEMain extends Application {
   ConsoleOutputTextArea consoleOutputTextArea;
   BalfScrollbar consoleScrollbar;
   Button runBtn;
+  CheckMenuItem toggleTheme;
+
 
 
   public static Font loadAndRegister(String resourcePath) {
@@ -79,6 +82,11 @@ public class ZIDEMain extends Application {
     stage.initStyle(StageStyle.UNDECORATED);
 
     _stage = stage;
+
+    stage.getIcons().add(
+            new Image(Objects.requireNonNull(getClass().getResourceAsStream("/files/balflaf_fx/icons/jb.png")))
+    );
+
 
     runtime = new ZPERuntimeEnvironment();
 
@@ -200,12 +208,29 @@ public class ZIDEMain extends Application {
     );
 
     var view = new Menu("_View");
-    var toggleTheme = new CheckMenuItem("Dark theme");
+    toggleTheme = new CheckMenuItem("Dark theme");
     toggleTheme.setOnAction(_ -> {
+      BalfLafManager.getInstance().toggleDarkMode(toggleTheme.isSelected());
       var scene = toggleTheme.getParentPopup().getOwnerWindow().getScene();
       scene.getRoot().pseudoClassStateChanged(javafx.css.PseudoClass.getPseudoClass("dark"),
               toggleTheme.isSelected());
-      BalfLafManager.getInstance().toggleDarkMode(true);
+
+      for (Tab t : editorTabs.getTabs()) {
+        EditorTab tab = (EditorTab) t;
+        if(toggleTheme.isSelected()) {
+          tab.switchOnDarkMode();
+        } else{
+          tab.switchOffDarkMode();
+        }
+        int scrollPosition = tab.getScrollPane().getVerticalScrollBar().getValue();
+        SwingUtilities.invokeLater(() -> {
+          tab.getScrollPane().getVerticalScrollBar().setValue(scrollPosition);
+        });
+
+      }
+
+
+
 
     });
     view.getItems().add(toggleTheme);
@@ -390,6 +415,7 @@ public class ZIDEMain extends Application {
 
         mainSyntax.setFontSize(14);
 
+
         Font jbMono = loadAndRegister("/files/JetBrainsMono-Regular.ttf");
         Font editorFont = jbMono.deriveFont(Font.PLAIN, 14);
         mainSyntax.setFont(editorFont);
@@ -400,38 +426,24 @@ public class ZIDEMain extends Application {
           lang = "yass";
         }
 
-        if(lang.equals("yass")) {
-
-          mainSyntax.resetKeywords(ZPEKit.getKeywordSet());
-          mainSyntax.setQuotes("\"'`");
-          mainSyntax.setVariableSymbol("$");
-
-          for (String keyword : ZPEKit.getKeywords()) {
-            mainSyntax.addAutoCompleteItem(keyword, CodeEditorView.AutoCompleteItemType.Keyword);
-          }
-          for (String keyword : ZPEKit.getTypeKeywords()) {
-            mainSyntax.addAutoCompleteItem(keyword, CodeEditorView.AutoCompleteItemType.Type);
-          }
-          for (String function : ZPEKit.getBuiltInFunctions()) {
-            mainSyntax.addAutoCompleteItem(function, CodeEditorView.AutoCompleteItemType.Function);
-          }
-          for (String s : ZPEInstance.getBuiltInStructuresNames()) {
-            mainSyntax.addAutoCompleteItem(s, CodeEditorView.AutoCompleteItemType.Type);
-          }
-        }
+        setLanguage(lang, mainSyntax);
 
 
 
+
+
+        Color dark = Color.decode("#282D37");
         // Wrapper + padding
-        JPanel wrapper = new JPanel(new BorderLayout());
+        BalfPanel wrapper = new BalfPanel(new BorderLayout());
         wrapper.setOpaque(true);
-        wrapper.setBackground(Color.WHITE);
         wrapper.setBorder(BorderFactory.createEmptyBorder(12, 14, 12, 14));
+        wrapper.setBackground(Color.white);
+        wrapper.setDarkColour(dark);
 
 
         BalfScrollbar scrollPane = new BalfScrollbar();
         scrollPane.setLightColour(Color.white);
-
+        scrollPane.setDarkColour(dark);
         scrollPane.setBorder(BorderFactory.createEmptyBorder());
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
@@ -469,13 +481,14 @@ public class ZIDEMain extends Application {
 
         editorRef.set(mainSyntax);
         swingNode.setContent(wrapper);
+        swingNode.getStyleClass().add("swing-editor-host");
 
         mainSyntax.setFont(new Font(mainSyntax.getFont().getFontName(), mainSyntax.getFont().getStyle(), mainSyntax.getFont().getSize()));
         mainSyntax.repaint();
 
 
         // Create the tab using the boolean (no cross-thread Swing calls)
-        Tab tab = createEditorTab(name, mainSyntax, file, swingNode, hasNonBlankContent::get);
+        Tab tab = createEditorTab(name, mainSyntax, scrollPane, file, swingNode, hasNonBlankContent::get);
         if(file != null){
           tab.setId(file);
         }
@@ -494,6 +507,40 @@ public class ZIDEMain extends Application {
 
   }
 
+  private void setLanguage(String lang, CodeEditorView mainSyntax) {
+    if(lang.equals("yass")) {
+      String[] keywords = ZPEKit.getBuiltInFunctions();
+
+      for (String s : keywords) {
+        mainSyntax.setTooltipInfo(s, getTooltipFunctionInfo(s));
+      }
+
+      mainSyntax.resetKeywords(ZPEKit.getKeywordSet());
+      mainSyntax.setQuotes("\"'`");
+      mainSyntax.setVariableSymbol("$");
+
+      for (String keyword : ZPEKit.getKeywords()) {
+        mainSyntax.addAutoCompleteItem(keyword, CodeEditorView.AutoCompleteItemType.Keyword);
+      }
+      for (String keyword : ZPEKit.getTypeKeywords()) {
+        mainSyntax.addAutoCompleteItem(keyword, CodeEditorView.AutoCompleteItemType.Type);
+      }
+      for (String function : keywords) {
+        mainSyntax.addAutoCompleteItem(function, CodeEditorView.AutoCompleteItemType.Function);
+      }
+      for (String s : ZPEInstance.getBuiltInStructuresNames()) {
+        mainSyntax.addAutoCompleteItem(s, CodeEditorView.AutoCompleteItemType.Type);
+      }
+
+
+      for (String s : ZPEKit.getAllCommands()) {
+        BalfSearchBox.SearchSuggestion suggestion = new BalfSearchBox.SearchSuggestion(s + " " + ZPEKit.getFunctionManualEntry(s), s);
+      }
+
+      rightFooterLabel.setText("YAS");
+    }
+  }
+
   private boolean confirmClose(String tabTitle) {
 
     Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
@@ -509,8 +556,8 @@ public class ZIDEMain extends Application {
     return alert.showAndWait().orElse(cancel) == close;
   }
 
-  private Tab createEditorTab(String title, CodeEditorView syntax, String path, Node content, Supplier<Boolean> hasContent) {
-    Tab tab = new EditorTab(title, path, syntax, content);
+  private Tab createEditorTab(String title, CodeEditorView syntax, BalfScrollbar scrollPane, String path, Node content, Supplier<Boolean> hasContent) {
+    EditorTab tab = new EditorTab(title, path, syntax, scrollPane, content);
     tab.setContent(content);
 
     // Disable JavaFX built-in close button
@@ -526,6 +573,10 @@ public class ZIDEMain extends Application {
     closeBtn.setMinSize(10, 10);
     closeBtn.setPrefSize(10, 10);
     closeBtn.setMaxSize(10, 10);
+
+    if(toggleTheme.isSelected()){
+      tab.switchOnDarkMode();
+    }
 
 
 
@@ -572,6 +623,8 @@ public class ZIDEMain extends Application {
 
       // Swing scrolling (important!)
       consoleScrollbar = new BalfScrollbar(consoleOutputTextArea);
+      consoleScrollbar.setLightColour(Color.WHITE);
+      consoleScrollbar.setDarkColour(Color.BLACK);
       consoleScrollbar.setBorder(BorderFactory.createEmptyBorder());
       consoleScrollbar.getVerticalScrollBar().setUnitIncrement(16);
 
@@ -606,6 +659,11 @@ public class ZIDEMain extends Application {
     var left = new Label("Ready");
     var centre = new Label("Ln 1, Col 1");
     rightFooterLabel = new Label("Text");
+
+    rightFooterLabel.addEventFilter(MouseEvent.MOUSE_CLICKED, e -> {
+      CodeEditorView c = ((EditorTab) editorTabs.getSelectionModel().getSelectedItem()).getEditor();
+      setLanguage("yass", c);
+    });
 
     var bar = new HBox(left, new Region(), centre, new Region(), rightFooterLabel);
     HBox.setHgrow(bar.getChildren().get(1), Priority.ALWAYS);
@@ -699,5 +757,146 @@ public class ZIDEMain extends Application {
 
   private static boolean hasDummyChild(TreeItem<File> item) {
     return item.getChildren().size() == 1 && item.getChildren().get(0).getValue() == null;
+  }
+
+  private String getTooltipFunctionInfo(String functionName) {
+    String output = "";
+
+
+    if (toggleTheme.isSelected()) {
+      output += "<html><div style='padding:10px;width:300px;color:#ddd;'>";
+    } else {
+      output += "<html><div style='padding:10px;width:300px;color:#333;'>";
+    }
+
+    ArrayList<AbstractMap.SimpleEntry<String, String>> params = getParams(ZPEKit.getFunctionManualHeader(functionName));
+
+    StringBuilder header = new StringBuilder();
+    for (int i = 0; i < params.size(); i++) {
+      AbstractMap.SimpleEntry<String, String> param = params.get(i);
+      String name = param.getKey();
+      String type = param.getValue();
+
+      //new Color(105, 143, 163) new Color(150, 0, 150)
+
+      if (toggleTheme.isSelected()) {
+        header.append("<span style='color: rgb(105, 143, 163);font-style:italic;'>").append(type).append("</span>").append(" <span style='color:#f60'>").append(name).append("</span>");
+      } else {
+        header.append("<span style='color: rgb(2, 87, 172);font-style:italic;'>").append(type).append("</span>").append(" <span style='color:#f60'>").append(name).append("</span>");
+      }
+
+      if (i + 1 < params.size()) {
+        header.append(", ");
+      }
+
+    }
+
+
+
+    if (toggleTheme.isSelected()) {
+      output += "<div style='margin-bottom:5px;'><code style='font-size:10px;'><span style='font-weight:bold;color:rgb(198, 120, 222)'>" + functionName + "</span> (" + header + ") : " + ZPEHelperFunctions.typeByteToString(ZPEKit.getFunctionReturnType(functionName)) + "</code></div>";
+    } else {
+      output += "<div style='margin-bottom:5px;'><code style='font-size:10px;'><span style='font-weight:bold;margin-bottom:20px;color:rgb(135, 16, 148)'>" + functionName + "</span> (" + header + ") : " + ZPEHelperFunctions.typeByteToString(ZPEKit.getFunctionReturnType(functionName)) + "</code></div>";
+    }
+
+    output += ZPEKit.getFunctionManualEntryAsStyledHtml(functionName, toggleTheme.isSelected());
+
+    if (!toggleTheme.isSelected()) {
+      output += "<div style='margin:10px 0; color:#222;'>Function version " + ZPEKit.getFunctionVersion(functionName) + "</div>";
+    } else {
+      output += "<div style='margin:10px 0; color:#bbb;'>Function version " + ZPEKit.getFunctionVersion(functionName) + "</div>";
+    }
+
+    output += "<div style='font-weight:100;margin-bottom:10px;'>Category: " + ZPEKit.getFunctionCategory(functionName) + "</div>";
+
+    output += "<div style='color:#0af'>Click for more information online.</div>";
+
+    output += "</div></html>";
+
+    return output;
+  }
+
+  static ArrayList<AbstractMap.SimpleEntry<String, String>> getParams(String function) {
+
+    //Proper parameter parser
+    ArrayList<AbstractMap.SimpleEntry<String, String>> array = new ArrayList<>();
+
+    int i = 0;
+
+    while (i < function.length()) {
+      if (function.charAt(i) == '(') {
+        i++;
+        break;
+      }
+      i++;
+    }
+
+    boolean optional = false;
+
+    while (i < function.length() && function.charAt(i) != ')') {
+
+
+      if (function.charAt(i) == '[') {
+        optional = true;
+        i++;
+      }
+
+      if (function.charAt(i) == '{') {
+
+
+        //This is a data type
+        StringBuilder dataType = new StringBuilder();
+        StringBuilder name = new StringBuilder();
+        i++;
+        while (i < function.length() && function.charAt(i) != '}') {
+          dataType.append(function.charAt(i));
+          i++;
+        }
+        i++;
+
+        while (i < function.length() && function.charAt(i) == ' ') {
+          i++;
+        }
+
+        while (i < function.length() && (function.charAt(i) != ',' && function.charAt(i) != ')' && function.charAt(i) != '[' && function.charAt(i) != ']' && function.charAt(i) != '.')) {
+          name.append(function.charAt(i));
+          i++;
+        }
+
+        while (i < function.length() && function.charAt(i) == '.') {
+          i++;
+        }
+
+        if (function.charAt(i) == '[') {
+
+          i--;
+        }
+
+        AbstractMap.SimpleEntry<String, String> entry;
+
+        /*if(optional){
+          entry = new AbstractMap.SimpleEntry<>("[" + name.toString() + "]", dataType.toString());
+        } else{*/
+        entry = new AbstractMap.SimpleEntry<>(name.toString(), dataType.toString());
+        //}
+
+
+        array.add(entry);
+
+
+        if (function.charAt(i) == ']') {
+          optional = false;
+          i++;
+        }
+
+      }
+      //System.out.println(function.substring(i));
+
+
+      i++;
+
+
+    }
+    return array;
   }
 }
