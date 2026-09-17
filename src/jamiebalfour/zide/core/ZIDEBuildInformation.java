5020fe623e1febf76613bd6d2c605d57640e9111
Jamie Balfour <jamie@balfour.scot>
2026-09-17 21:21:58 +0100

    * Fixed an issue with the Windows UI.

--- src/jamiebalfour/zide/core/ZIDEBuildInformation.java
+++ src/jamiebalfour/zide/core/ZIDEBuildInformation.java
@@ -3,8 +3,8 @@ package jamiebalfour.zide.core;
 class ZIDEBuildInformation {
 
   static String date = "17-09-2026";
-  static String time = "20:19:37";
-  static String build = "4802";
+  static String time = "21:21:36";
+  static String build = "4805";
 
 }
 
