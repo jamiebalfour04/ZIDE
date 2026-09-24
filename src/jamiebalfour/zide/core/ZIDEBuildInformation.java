2fc82f4ddce5693e90bcd34fe4f94b1e50650c5f
Jamie Balfour <jamie@balfour.scot>
2026-09-24 10:58:19 +0100

    Update ZIDE build metadata and packaged artifact

--- src/jamiebalfour/zide/core/ZIDEBuildInformation.java
+++ src/jamiebalfour/zide/core/ZIDEBuildInformation.java
@@ -2,9 +2,9 @@ package jamiebalfour.zide.core;
 
 class ZIDEBuildInformation {
 
-  static String date = "23-09-2026";
-  static String time = "21:25:56";
-  static String build = "5024";
+  static String date = "24-09-2026";
+  static String time = "07:43:56";
+  static String build = "5035";
 
 }
 
